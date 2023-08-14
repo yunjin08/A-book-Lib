9ba2cf3fe05b2d59f7c39238c33a043366c24d66
Yunjin <jed.donaire08@gmail.com>
2023-08-14 21:58:27 +0800

    Added Style for Clicking addButton

--- public/script.js
+++ public/script.js
@@ -8,4 +8,22 @@ function addBooktoMyLibrary (){
 
 }
 
+const popUp = document.querySelector('#popup');
+const jsButton = document.querySelector('.js-button');
+const jsOverlay = document.querySelector('#overlay');
+
+jsButton.addEventListener ('click', () => {
+    popUp.classList.remove('hidden');
+    jsOverlay.classList.remove('hidden');
+});
+
+
+jsOverlay.addEventListener ('click', () => {
+    if(event.target === jsOverlay){
+        popUp.classList.add('hidden');
+        jsOverlay.classList.add('hidden');
+    }
+});
+
+
 
